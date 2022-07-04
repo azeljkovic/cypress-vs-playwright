d50758bdc5d0d9db18a18f87686d5ddc96ff31f3
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-07-04 16:07:41 +0200

    added first cypress test

---
+++ cypress/e2e/real-world-app/test.cy.js
@@ -0,0 +1,9 @@
+describe('simple test', () => {
+  it('login via UI', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-test="signin-username"]').type('Katharina_Bernier');
+    cy.get('[data-test="signin-password"]').type('s3cret');
+    cy.get('[data-test="signin-submit"]').click();
+  });
+});
+

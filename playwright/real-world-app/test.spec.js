cc2fc9ef2edbd2dfa10980bf13398d9c7ca0f6c2
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-07-06 15:02:38 +0200

    added first playwright test

---
+++ playwright/real-world-app/test.spec.js
@@ -0,0 +1,10 @@
+const { test } = require('@playwright/test');
+
+test('login via UI', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await page.fill('#username', 'Katharina_Bernier');
+  await page.fill('#password', 's3cret');
+  await page.click('[data-test="signin-submit"]');
+  // wait for 3 seconds to see what's happening in headed mode
+  await page.waitForTimeout(3000);
+});

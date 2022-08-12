a7a561e7e702774ed868b6538603fdc2b1dc5750
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-08-12 16:14:49 +0200

    added custom selectors

--- playwright/real-world-app/request.spec.ts
+++ playwright/real-world-app/request.spec.ts
@@ -4,15 +4,15 @@ import mainSelectors from '../fixtures/selectors/main.json';
 
 test('should do a login API call', async ({ request }) => {
   // send login request
-  const rsp = await request.post('http://localhost:3001/login', {
+  const response = await request.post('http://localhost:3001/login', {
     data: {
-      password: 's3cret',
-      username: 'Katharina_Bernier',
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
     },
   });
-  let body = await rsp.json();
-  expect(rsp.ok()).toBeTruthy();
-  expect(body.user.email).toEqual('Norene39@yahoo.com');
+  let body = await response.json();
+  expect(response.ok()).toBeTruthy();
+  expect(body.user.email).toEqual(process.env.EMAIL);
 });
 
 test('make a transaction and wait for the response', async ({ page }) => {
@@ -23,15 +23,15 @@ test('make a transaction and wait for the response', async ({ page }) => {
   await page.click(loginSelectors.submit);
   await expect(page.locator(mainSelectors.usernameLabel)).not.toBeVisible();
 
-  await page.click('[data-test="nav-top-new-transaction"]');
-  await expect(page.locator('[data-test="users-list"] > li')).toHaveCount(4);
-  await page.click('[data-test="users-list"]:nth-child(2)');
-  await page.fill('#amount', '5');
-  await page.fill('#transaction-create-description-input', 'big bucks');
-  await page.click('[data-test="transaction-create-submit-payment"]');
+  await page.click(mainSelectors.newTransactionButton);
+  await expect(page.locator(mainSelectors.usersList)).toHaveCount(4);
+  await page.click(mainSelectors.user);
+  await page.fill(mainSelectors.transactionAmount, '5');
+  await page.fill(mainSelectors.transactionDescription, 'big bucks');
+  await page.click(mainSelectors.submitTransactionButton);
   await page.waitForResponse('http://localhost:3001/transactions');
 
-  await expect(page.locator('.MuiAlert-message')).toHaveText('Transaction Submitted!');
+  await expect(page.locator(mainSelectors.notificationsBadge)).toHaveText('Transaction Submitted!');
 
   // wait for 3 seconds to see what's happening in headed mode
   await page.waitForTimeout(2000);

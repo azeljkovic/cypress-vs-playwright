2692a1fa04b9ae48ad330cd83003a43a31c99b1f
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-08-12 14:10:43 +0200

    fixed eslint issues in Cypress tests

--- cypress/e2e/real-world-app/intercept.cy.ts
+++ cypress/e2e/real-world-app/intercept.cy.ts
@@ -1,52 +1,54 @@
 import object from '../../fixtures/xstate.json';
-import notificationsObj from'../../fixtures/notifications.json';
+import notificationsObj from '../../fixtures/notifications.json';
 
 describe('API requests', () => {
-    it('intercept request', () => {
-        cy.intercept('POST', '/transactions').as('transaction');
+  it('intercept request', () => {
+    cy.intercept('POST', '/transactions').as('transaction');
 
-        // login via API
-        cy.request('POST', 'http://localhost:3001/login', {
-            password: 's3cret',
-            remember: true,
-            type: 'LOGIN',
-            username: 'Katharina_Bernier',
-        });
-        localStorage.setItem("authState", JSON.stringify(object));
-
-        cy.visit('http://localhost:3000');
-        cy.get('[data-test="nav-top-new-transaction"]').click();
-        cy.get('[data-test="users-list"]').children().should('have.length', 4);
-        cy.get('[data-test="users-list"]').children().eq(2).click();
-        cy.get('#amount').type('5');
-        cy.get('#transaction-create-description-input').type('big bucks');
-        cy.get('[data-test="transaction-create-submit-payment"]').click();
-
-        cy.wait('@transaction').should(({ request, response }) => {
-            expect(response.statusCode).to.equals(200);
-            expect(response.body.transaction.status).to.equals("complete");
-        });
+    // login via API
+    cy.request('POST', 'http://localhost:3001/login', {
+      password: 's3cret',
+      remember: true,
+      type: 'LOGIN',
+      username: 'Katharina_Bernier',
     });
+    localStorage.setItem('authState', JSON.stringify(object));
 
-    it('stub the response', () => {
-        cy.intercept('GET', '/notifications',
-            {
-                statusCode: 200,
-                body: notificationsObj,
-                delayMs: 50,
-            }).as('notifications');
+    cy.visit('http://localhost:3000');
+    cy.get('[data-test="nav-top-new-transaction"]').click();
+    cy.get('[data-test="users-list"]').children().should('have.length', 4);
+    cy.get('[data-test="users-list"]').children().eq(2).click();
+    cy.get('#amount').type('5');
+    cy.get('#transaction-create-description-input').type('big bucks');
+    cy.get('[data-test="transaction-create-submit-payment"]').click();
 
-        // login via API
-        cy.request('POST', 'http://localhost:3001/login', {
-            password: 's3cret',
-            remember: true,
-            type: 'LOGIN',
-            username: 'Katharina_Bernier',
-        });
-        localStorage.setItem("authState", JSON.stringify(object));
-
-        cy.visit('http://localhost:3000');
-        cy.wait('@notifications');
-        cy.get('[data-test="nav-top-notifications-count"] > .MuiBadge-badge').should('have.text', '99+')
+    cy.wait('@transaction').should(({ response }) => {
+      expect(response.statusCode).to.equals(200);
+      expect(response.body.transaction.status).to.equals('complete');
     });
+  });
+
+  it('stub the response', () => {
+    cy.intercept('GET', '/notifications', {
+      statusCode: 200,
+      body: notificationsObj,
+      delayMs: 50,
+    }).as('notifications');
+
+    // login via API
+    cy.request('POST', 'http://localhost:3001/login', {
+      password: 's3cret',
+      remember: true,
+      type: 'LOGIN',
+      username: 'Katharina_Bernier',
+    });
+    localStorage.setItem('authState', JSON.stringify(object));
+
+    cy.visit('http://localhost:3000');
+    cy.wait('@notifications');
+    cy.get('[data-test="nav-top-notifications-count"] > .MuiBadge-badge').should(
+      'have.text',
+      '99+',
+    );
+  });
 });
--- cypress/e2e/real-world-app/request.cy.ts
+++ cypress/e2e/real-world-app/request.cy.ts
@@ -1,18 +1,18 @@
 describe('make API requests', () => {
-    it('basic API request', () => {
-        cy.request('POST', 'http://localhost:3001/login', {
-            password: 's3cret',
-            username: 'Katharina_Bernier',
-        });
+  it('basic API request', () => {
+    cy.request('POST', 'http://localhost:3001/login', {
+      password: 's3cret',
+      username: 'Katharina_Bernier',
     });
+  });
 
-    it('API login test', () => {
-        cy.request('POST', 'http://localhost:3001/login', {
-            password: 's3cret',
-            username: 'Katharina_Bernier',
-        }).should((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body.user.email).to.eq('Norene39@yahoo.com');
-        });
+  it('API login test', () => {
+    cy.request('POST', 'http://localhost:3001/login', {
+      password: 's3cret',
+      username: 'Katharina_Bernier',
+    }).should(response => {
+      expect(response.status).to.eq(200);
+      expect(response.body.user.email).to.eq('Norene39@yahoo.com');
     });
+  });
 });

e344ce6afe99a0e36657dfe7783596d7b9c328df
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-08-16 15:11:42 +0200

    added additional cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    chromeWebSecurity: false,
-    // TODO add more config
+    watchForFileChanges: true,
+    retries: 1,
+    viewportHeight: 768,
+    viewportWidth: 1024,
+    videoCompression: 0,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
